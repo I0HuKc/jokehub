caeed028ba11e774dd52fc7998a2d4e958e127d0
I0HuKc <web-disain@mail.ua>
2022-05-11 19:10:08 +0300

    .

--- backend/src/server/mod.rs
+++ backend/src/server/mod.rs
@@ -14,7 +14,10 @@ pub trait Server {
 
 impl Server for Rocket<Build> {
     fn launch(self) -> Self {
-        self.manage_postgres().manage_mongodb().mount(
+        self
+            .manage_postgres()
+            .manage_mongodb()
+            .mount(
             "/v1",
             rocket::routes![create_joke, create_anecdote, get_anecdote],
         )
