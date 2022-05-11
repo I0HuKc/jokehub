781dc06564e78d4b5df3821f1507e0198da33a20
I0HuKc <web-disain@mail.ua>
2022-05-11 14:08:07 +0300

    Добавл создание индекса в иницализационный скрипт

--- tools/mongodb_init.sh
+++ tools/mongodb_init.sh
@@ -2,13 +2,31 @@
 
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
     db.createUser({
-    user: "$MONGO_USER",
-    pwd: "$MONGO_USER_PASSWORD",
-    roles: [
-        {
-        role: "$MONGO_USER_ROLE",
-        db: "$MONGO_INITDB_DATABASE",
-        },
-    ],
+        user: "$MONGO_USER",
+        pwd: "$MONGO_USER_PASSWORD",
+        roles: [
+            {
+                role: "$MONGO_USER_ROLE",
+                db: "$MONGO_INITDB_DATABASE",
+            },
+        ],
     });
+EOF
+
+mongo --username $MONGO_USER --password $MONGO_USER_PASSWORD --authenticationDatabase $MONGO_INITDB_DATABASE $MONGO_INITDB_DATABASE <<EOF
+    db.createCollection("$MONGO_ANECDOTE_COLLECTION");
+
+    db.$MONGO_ANECDOTE_COLLECTION.createIndex(
+        {
+            "text": 1
+        }, 
+        {
+            "unique": true, 
+            "partialFilterExpression": {
+                "text": {
+                    \$type: "string"
+                }
+            }
+        }
+    );
 EOF
