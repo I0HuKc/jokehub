ce3db7efc56886122d9e671bfb3385773bfee8f1
I0HuKc <web-disain@mail.ua>
2022-05-12 18:14:06 +0300

    Добавил создание и индексирование колекци `punch`

--- tools/mongodb_init.sh
+++ tools/mongodb_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Создание пользователя для БД
 mongo -- "$MONGO_INITDB_DATABASE" <<EOF
     db.createUser({
         user: "$MONGO_USER",
@@ -13,9 +14,9 @@ mongo -- "$MONGO_INITDB_DATABASE" <<EOF
     });
 EOF
 
+# Создание коллекций и установка индексов
 mongo --username $MONGO_USER --password $MONGO_USER_PASSWORD --authenticationDatabase $MONGO_INITDB_DATABASE $MONGO_INITDB_DATABASE <<EOF
     db.createCollection("anecdote");
-
     db.anecdote.createIndex(
         {
             "text": 1
@@ -26,6 +27,26 @@ mongo --username $MONGO_USER --password $MONGO_USER_PASSWORD --authenticationDat
                 "text": {
                     \$type: "string"
                 }
+            }
+        }
+    );
+
+    db.createCollection("punch");
+    db.punch.createIndex(
+        {
+            "setup": 1,
+            "punchline": 1,
+        }, 
+        {
+            "unique": true, 
+            "partialFilterExpression": {
+                "setup": {
+                    \$type: "string"
+                },
+                
+                "punchline": {
+                    \$type: "string"
+                },
             }
         }
     );
