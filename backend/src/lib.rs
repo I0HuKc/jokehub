0a23720d9b2be0348e272cedc093dc0dad294d55
I0HuKc <web-disain@mail.ua>
2022-06-03 00:12:31 +0300

    Расширил стандартный  `Vec<T>` методом конвертации

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,10 +1,27 @@
 pub mod db;
+pub mod errors;
 pub mod model;
 pub mod server;
-pub mod errors;
 
 #[macro_use]
 extern crate rocket;
 
 #[macro_use]
 extern crate dotenv_codegen;
+
+pub trait VectorConvert<F> {
+    fn convert<T>(from: Vec<F>) -> Vec<T>
+    where
+        T: From<F>;
+}
+
+impl<F> VectorConvert<F> for Vec<F> {
+    fn convert<T>(from: Vec<F>) -> Vec<T>
+    where
+        T: From<F>,
+    {
+        let mut result: Vec<T> = Vec::new();
+        from.into_iter().for_each(|item| result.push(item.into()));
+        result
+    }
+}
